35661d7539c0d23792176ac287faffaecbef18ba
Oleksandr Polishchuk <olexandr.polishchuk@3dlook.me>
2021-01-14 18:18:42 +0200

    Added a new file

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -12,7 +12,7 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
-        Grid(items: viewModel.cards) { card in
+        Grid(viewModel.cards) { card in
                 CardView(card: card).onTapGesture {
                     viewModel.choose(card: card)
                 }
--- Memorize/Grid.swift
+++ Memorize/Grid.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
     var items: [Item]
     var viewForItem: (Item) -> ItemView
+    
+    init(_ items: [Item], viewForItem: @escaping (Item) -> ItemView) {
+        self.items = items
+        self.viewForItem = viewForItem
+    }
 
     var body: some View {
         GeometryReader { geometry in
